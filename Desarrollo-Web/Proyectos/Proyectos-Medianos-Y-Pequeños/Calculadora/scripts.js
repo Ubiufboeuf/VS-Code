cdec9bee1e40e69f32d052a4e2fd2df7e5f8f4c6
mAngo-mAestro <fede.2007.artencio.u@gmail.com>
2024-04-09 13:09:41 -0300

    resp 09/04/2024 - 13:10

--- Desarrollo-Web/Proyectos/Proyectos-Medianos-Y-Pequeños/Calculadora/scripts.js
+++ Desarrollo-Web/Proyectos/Proyectos-Medianos-Y-Pequeños/Calculadora/scripts.js
@@ -1,32 +1,90 @@
 const btnNumbers = document.querySelectorAll(".btnNumbers");
 const btnSymbols = document.querySelectorAll(".btnSymbols");
-const erase = document.querySelector("#eraseAll");
+const borrarTodo = document.querySelector("#eraseAll");
+const borrar = document.querySelector("#erase");
 const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
 
-let numPrev;
-let resultado;
-let pantalla = "";
+let mensajes = {
+    cadena_vacía: "La cadena de números está vacía.",
+    doble_simbolo: "No puedes usar más de 1 símbolo a la vez.",
+};
 
+h1.textContent = "0";
+let cadena = ""; // Toda la cadena ingresada
+let lastSim = "";
 
-btnNumbers.forEach(btn => {
-    btn.addEventListener("click", () => {
-        pantalla = pantalla + btn.textContent;
-        h1.textContent = pantalla;
-    })
-})
+// Eventos - - - - - - - - - -
 
-btnSymbols.forEach(symbol => {
-    symbol.addEventListener("click", () => {
-        numPrev = pantalla;
-        pantalla += symbol.textContent;
-        h1.textContent = pantalla;
-    })
-})
+btnNumbers.forEach((numero) => {
+    numero.addEventListener("click", (event) => {
+        numeros(numero.value);
+    });
+});
 
-erase.addEventListener("click", () => {
-    num1 = 0;
-    num2 = 0;
-    resultado = 0;
-    pantalla = "";
-    h1.textContent = pantalla;
-})
+btnSymbols.forEach((simbolo) => {
+    simbolo.addEventListener("click", () => {
+        simbolos(simbolo.value);
+    });
+});
+
+borrar.addEventListener("click", back);
+borrarTodo.addEventListener("click", clear);
+
+// Funciones - - - - - - - - - -
+
+function refresco(cadena) {
+    h1.textContent = cadena;
+    h1.textContent = h1.textContent == "" ? "0" : cadena;
+
+    p.textContent = "[Advertencias]";
+}
+
+function back() {
+    cadena = cadena.substring(0, cadena.length - 1);
+    refresco(cadena);
+}
+
+function clear() {
+    cadena = "";
+    refresco(cadena);
+}
+
+function numeros(num) {
+    cadena += num;
+    refresco(cadena);
+}
+
+function simbolos(sim) {
+    // Si la cadena no está vacía, cadena != ""
+    if (cadena != "") {
+        // Si el símbolo no es el igual (=), sim != "="
+        if (sim != "=") {
+            if (
+                lastSim != "+" &&
+                lastSim != "-" &&
+                lastSim != "*" &&
+                lastSim != "/"
+            ) {
+                lastSim = sim;
+                console.log(lastSim);
+                switch (sim) {
+                    case "+":
+                        cadena += sim;
+                        refresco(cadena);
+                        break;
+                }
+            } else {
+                console.log(mensaje[1]);
+            }
+        }
+        // Si el símbolo es el igual (=), sim == "="
+        else {
+            // Contenido
+        }
+    }
+    // Si la cadena está vacía, cadena == ""
+    else {
+        p.textContent = mensajes.cadena_vacía;
+    }
+}
